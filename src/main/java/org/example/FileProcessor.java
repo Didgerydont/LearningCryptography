1aa983d37933fb5abcb1f71f35775c09ed32eddf
Will Prendergast <prendygast@hotmail.com>
2023-11-10 23:19:00 +0000

    File processing working as expected. Adding LanguageDetector class

--- src/main/java/org/example/FileProcessor.java
+++ src/main/java/org/example/FileProcessor.java
@@ -1,6 +1,49 @@
 package org.example;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 // used to process file containing all English words for EnglishLanguageDetector
 public class FileProcessor {
+
+    private List<String> words;
+
+    public FileProcessor() {
+        this.words = new ArrayList<>();
+        getData();
+    }
+
+    private void getData() {
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            fileReader = new FileReader(new File("src/main/resources/EnglishWords.txt"));
+            bufferedReader = new BufferedReader(fileReader);
+
+            String line = "";
+
+            while((line=bufferedReader.readLine()) != null){
+                words.add(line.toUpperCase());
+                System.out.println(line);
+            }
+
+            fileReader.close();
+            bufferedReader.close();
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public List<String> getWords() {
+        return this.words;
+    }
+
 
 }
---
+++ src/main/java/org/example/FileProcessorApp.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class FileProcessorApp {
+    public static void main(String[] args){
+        FileProcessor file = new FileProcessor();
+        file.getWords();
+    }
+}
